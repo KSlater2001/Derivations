30d25074b59e81e904c55ff48beead9fa9e0f69a
s133751 <s133751@WVH-144-84723.fsd.domain>
2017-12-12 08:08:09 -0900

    creating non-static objects

--- src/derive/DeriveRelation.java
+++ src/derive/DeriveRelation.java
@@ -15,6 +15,7 @@ public class DeriveRelation {
 		String derivative = funcType(relation);
 		System.out.println(derivative);
 	}
+	
 	public static String funcType(String relation){
 
 		if(relation.charAt(relation.indexOf('=') + 2) != '-') {
@@ -41,9 +42,9 @@ public class DeriveRelation {
 					}
 					terms.add(relation.substring(i, i + k));	
 					i += k;
-				} //end else statement
+				} //end 'else' statement
 			}
-		} //end for loop
+		} //end 'for' loop
 		
 		//extracting unnecessary terms
 		for (int i = 0; i < terms.size(); i++){
@@ -55,6 +56,27 @@ public class DeriveRelation {
 		
 		String derivative = polynomials.splitPoly(terms);
 		return derivative;
+	}
+	
+	public static ArrayList<String> deriveE(ArrayList<String> logTerms){
+		//pre-condition: input ArrayList only contains logarithmic terms
+		//post-condition: outputs derived log terms
+		
+		//example log: ln(x), log(x + 2), log6(2x)
+		ArrayList<String> derivedLogs = new ArrayList<String>();
+		
+		//sending the different terms to methods that derive them
+		for(String i : logTerms) {
+			int k = 0;
+			if(i.contains("ln")) derivedLogs.add(deriveNaturalLog(logTerms.get(k - 1) + " " + i));
+			else if(i.contains("log") && i.charAt(i.indexOf("g") + 1) == '(') {
+				derivedLogs.add(deriveCommonLog(logTerms.get(k - 1) + " " + i));
+			}
+			else if(i.contains("e")) derivedLogs.add(deriveETerm(logTerms.get(k - 1) + " " + i));
+			else derivedLogs.add(deriveBasedLog(logTerms.get(k - 1) + " " + i));
+			k++;
+		}
+		
 	}
 }
 
--- src/executables/logarithimsAndE.java
+++ src/executables/logarithimsAndE.java
@@ -1,39 +1,19 @@
 package executables;
 import java.util.ArrayList;
 public class logarithimsAndE {
-	public static ArrayList<String> deriveE(ArrayList<String> logTerms){
-		//pre-condition: input ArrayList only contains logarithmic terms
-		//post-condition: outputs derived log terms
-		
-		//example log: ln(x), log(x + 2), log6(2x)
-		ArrayList<String> derivedLogs = new ArrayList<String>();
-		
-		//sending the different terms to methods that derive them
-		for(String i : logTerms) {
-			int k = 0;
-			if(i.contains("ln")) derivedLogs.add(deriveNaturalLog(logTerms.get(k - 1) + " " + i));
-			else if(i.contains("log") && i.charAt(i.indexOf("g") + 1) == '(') {
-				derivedLogs.add(deriveCommonLog(logTerms.get(k - 1) + " " + i));
-			}
-			else if(i.contains("e")) derivedLogs.add(deriveETerm(logTerms.get(k - 1) + " " + i));
-			else derivedLogs.add(deriveBasedLog(logTerms.get(k - 1) + " " + i));
-			k++;
-		}
-		
-	}
-	public static String deriveNaturalLog(String lnTerm){
+	public String deriveNaturalLog(String lnTerm){
 		//derives natural log terms
 		String denominator = lnTerm.substring(lnTerm.indexOf('(') + 1, lnTerm.lastIndexOf(')');
 		String numerator = DeriveRelation.funcType(denominator);
 		
 	}
-	public static String deriveCommonLog(String logTerm){
+	public String deriveCommonLog(String logTerm){
 		//derives common log terms
 	}
-	public static String deriveBasedLog(String logbTerm){
+	public String deriveBasedLog(String logbTerm){
 		//derives logs that contain a base
 	}
-	public static String deriveETerm(String eTerm) {
+	public String deriveETerm(String eTerm) {
 		//derives terms that contain e
 	}
 }
